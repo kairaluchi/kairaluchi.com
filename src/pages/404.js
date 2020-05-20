c46283af6de4fc0ad265117e0271b235aebf9a14
Alexander Obi <alexebube@gmail.com>
2020-05-20 15:56:41 -0400

    minor fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,27 @@
 import React from 'react'
-
+import styled from "styled-components";
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
+const AutoContainer = styled.div`
+  position: static;
+  max-width: 1200px;
+  padding: 40px 15px;
+  margin: 0 auto;
+  text-align: center;
+  
+  p {
+    font-size: 18px;
+  }
+`
+
 const NotFoundPage = props => (
-  <Layout page='Page Not Found' {...props}>
+  <Layout page='Page Not Found!' {...props}>
     <Seo title='404: Not found' />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <AutoContainer>
+      <h1>NOT FOUND!</h1>
+      <p>You just tried to access a resource that doesn&#39;t exist... the sadness.</p>
+    </AutoContainer>
   </Layout>
 )
 
