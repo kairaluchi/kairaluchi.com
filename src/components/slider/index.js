16a4934b1463955b5ff38f5da47bb1fe33077386
Alexander Obi <alexebube@gmail.com>
2020-05-20 10:08:29 -0400

    fix slider

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Slider from './slider'
+import slideOne from '../../images/slides/slide1.jpeg'
+import slideTwo from '../../images/slides/slide2.jpeg'
+import slideThree from '../../images/slides/slide3.jpeg'
+import slideFour from '../../images/slides/slide4.jpeg'
 
-const images = [
-  'https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
-  'https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80',
-  'https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80',
-  'https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80'
-]
+const images = [slideOne, slideTwo, slideThree, slideFour]
 
 const displays = [
   {
